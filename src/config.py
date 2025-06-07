fb3920e4436d9990d011081719deabab26f67328
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-06-07 23:39:01 +0200

    Allow overriding Postgres URL via POSTGRES_URL env variable

--- src/config.py
+++ src/config.py
@@ -1,11 +1,8 @@
 import os
 
 ENV = os.getenv("ENV", "local")
+POSTGRES_URL = os.getenv("POSTGRES_URL", "postgresql://user:password@localhost/db")
 
-DATABASE_URL = (
-    "sqlite:///./test.db"
-    if ENV == "local"
-    else "postgresql://user:password@localhost/db"
-)
+DATABASE_URL = "sqlite:///./test.db" if ENV == "local" else POSTGRES_URL
 
 TOKEN_SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
