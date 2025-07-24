c04f4e203c7b33dfb94ababf26dac543a0c8526b
olegsvs <oleg.texet@gmail.com>
2025-07-24 08:51:02 +0300

    Update uvicorn args

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,11 @@ echo "Initializing database..."
 python -m src.db.db_session
 
 echo "Starting FastAPI application..."
+WORKERS=${WORKERS:-2}
+TIMEOUT_GRACEFUL_SHUTDOWN=${TIMEOUT_GRACEFUL_SHUTDOWN:-30}
+
 if [ "$IPv6" = "false" ]; then
-    exec uvicorn src.main:app --host 0.0.0.0 --proxy-headers --forwarded-allow-ips="*"
+    exec uvicorn src.main:app --host 0.0.0.0 --proxy-headers --forwarded-allow-ips="*" --timeout-graceful-shutdown $TIMEOUT_GRACEFUL_SHUTDOWN --workers $WORKERS
 else
-    exec uvicorn src.main:app --host '::' --proxy-headers --forwarded-allow-ips="*"
+    exec uvicorn src.main:app --host '::' --proxy-headers --forwarded-allow-ips="*" --timeout-graceful-shutdown $TIMEOUT_GRACEFUL_SHUTDOWN --workers $WORKERS
 fi
